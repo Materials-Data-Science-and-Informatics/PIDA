0d2a5a5ab071a4b7cb95939d0d899efb9a61895f
Said Fathalla <21238109+saidfathalla@users.noreply.github.com>
2022-10-14 11:37:38 +0200

    Update PIDAscript.js

--- assets/PIDAscript.js
+++ assets/PIDAscript.js
@@ -1,26 +1,20 @@
-function UnCryptMailto(s, shift) {
-	var n=0;
-	var r="";
-	for(var i=0;i<s.length;i++) {
-		n=s.charCodeAt(i);
-		if (n>=8364) {n = 128;}
-		r += String.fromCharCode(n-(shift));
-	}
-	return r;
-}
-function linkTo_UnCryptMailto(s, shift)	{
-	location.href=UnCryptMailto(s, shift);
-}
-function CryptMailto() {
-	shift = 2;
-	var n=0;
-	var r="";
-	var s="mailto:"+document.forms[0].emailField.value;
-	for(var i=0;i<s.length;i++) { 
-		n=s.charCodeAt(i); 
-		if (n>=8364) {n = 128;}
-		r += String.fromCharCode(n+shift); 
-	}
-	document.forms[0].cyptedEmailField.value = r;
-	document.forms[0].HTMLCyptedEmailField.value = "<a href=\"javascript:linkTo_UnCryptMailto('"+r+"', "+shift+");\">eMail-Link</a>";
-}
+   function UnCryptMailto( s )
+    {
+        var n = 0;
+        var r = "";
+        for( var i = 0; i < s.length; i++)
+        {
+            n = s.charCodeAt( i );
+            if( n >= 8364 )
+            {
+                n = 128;
+            }
+            r += String.fromCharCode( n - 1 );
+        }
+        return r;
+    }
+
+    function linkTo_UnCryptMailto( s )
+    {
+        location.href=UnCryptMailto( s );
+    }
